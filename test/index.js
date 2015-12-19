d5bdd758d9ffaedcd71f995d159dd7313273e144
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-12-19 19:10:36 +0300

    simplification

--- test/index.js
+++ test/index.js
@@ -1,31 +1,14 @@
 const assert = require('assert');
-const path = require('path');
-
-const MemoryFileSystem = require('memory-fs');
-const Webpack = require('webpack');
-
-const config = require('./fixture/webpack.config');
+const config = require('./fixture/fixture.config');
+const transform = require('./fixture/transform');
 
 suite('webpack-loader', () => {
   test('should inline comment', done => {
-    const fs = new MemoryFileSystem();
-    const compiler = new Webpack(config);
-
-    compiler.outputFileSystem = fs;
-    compiler.run((er) => {
-      if (er) {
-        return void done(er);
-      }
-
-      const bundle = path.join(__dirname, './fixture/index.bundle.js');
-      const result = fs.readFileSync(bundle, 'utf8');
-
-      try {
+    transform(config)
+      .then(result => {
         assert.ok(result.indexOf('/* inlined comment */') > -1);
         done();
-      } catch (e) {
-        done(e);
-      }
-    });
+      })
+      .catch(done);
   });
 });
