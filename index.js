42186f3b04cee318ada3918e05dd4f5b15a96978
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-12-19 20:11:04 +0300

    small helper

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
  * @return {string}
  */
 module.exports = function (source) {
+  // In case of async transformations
+  // var callback = this.async();
+
+  // Flag itself cacheable if possible
+  // this.cacheable();
+  // Mark dependencies if a loader uses external resources
+  // this.addDependency(externalPath);
   return '/* inlined comment */\n' + source;
 };
